d0ba85f63d7b03cd72b046dfcf1aef2ff38b0436
gokkul22 <65940687+gokkul22@users.noreply.github.com>
2020-05-26 18:58:12 +0530

    created fibonacci series

---
+++ Fibonacci.py
@@ -0,0 +1,18 @@
+n=input("Enter n")                      #to find the fibonacci series till "n"
+x=0
+y=1
+print(x)
+while y < int(n) :
+    print(y)
+    x = y
+    y = x+y
+    
+    
+Output:
+
+Enter n10
+0
+1
+2
+4
+8
